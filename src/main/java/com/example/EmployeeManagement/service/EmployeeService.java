54e78a15e3274f535963c93512e913e2b01a3db0
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-08-14 16:49:55 +0100

    Create service layer

---
+++ src/main/java/com/example/EmployeeManagement/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.example.EmployeeManagement.service;
+
+import com.example.EmployeeManagement.entity.Employee;
+import com.example.EmployeeManagement.implementation.EmployeeImpl;
+
+import java.util.List;
+
+public class EmployeeService implements EmployeeImpl {
+
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+
+    public Employee getEmployee(long employeeId) {
+        return null;
+    }
+
+    public Employee addNewEmployee(Employee employee) {
+        return null;
+    }
+
+    public Employee updateEmployee(Long employeeId, Employee employee) throws Exception {
+        return null;
+    }
+
+    public void deleteEmployee(Long employeeId) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/example/EmployeeManagement/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.EmployeeManagement.service;
+
+import com.example.EmployeeManagement.entity.User;
+import com.example.EmployeeManagement.implementation.UserImpl;
+
+import java.util.List;
+
+public class UserService implements UserImpl {
+
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    public User getUser(long userId) {
+        return null;
+    }
+
+    public User addNewUser(User user) {
+        return null;
+    }
+
+    public User updateUser(Long userId, User user) throws Exception {
+        return null;
+    }
+
+    public void deleteUser(Long userId) throws Exception {
+
+    }
+}
