9e7da051a975e44e66f2cd12a99f87f408c8127d
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-08-22 20:22:21 +0100

    Add KeycloakLogoutHandler in Security config

--- src/main/java/com/example/EmployeeManagement/configuration/SecurityConfig.java
+++ src/main/java/com/example/EmployeeManagement/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.EmployeeManagement.configuration;
 
+import org.keycloak.adapters.springsecurity.authentication.KeycloakLogoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -11,27 +13,34 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    public SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
+                .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/error", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/users/**").hasRole("ADMIN")
                         .requestMatchers("/employees/**").hasAnyRole("ADMIN", "MANAGER")
                         .requestMatchers("/employees/view/**").hasRole("EMPLOYEE")
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .defaultSuccessUrl("/employees", true)
+                        .requestMatchers(new AntPathRequestMatcher("/"))
                         .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
-
+                        .anyRequest()
+                        .authenticated());
+        http.oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults()));
+        http.oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/"));
         return http.build();
     }
 
