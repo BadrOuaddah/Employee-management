426dafff2644f4ce4aef7f4d28c3b9dd65b9207b
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-09-11 12:36:33 +0100

    Create JWT converter

--- src/main/java/com/example/EmployeeManagement/configuration/SecurityConfig.java
+++ src/main/java/com/example/EmployeeManagement/configuration/SecurityConfig.java
@@ -1,31 +1,41 @@
 package com.example.EmployeeManagement.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtConverter jwtConverter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/error", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/users/**").hasRole("ADMIN")
-                        .requestMatchers("/employees/**").hasAnyRole("ADMIN", "MANAGER")
-                        .requestMatchers("/employees/view/**").hasRole("EMPLOYEE")
+                        .requestMatchers("/employees/**").hasAnyRole("admin", "manager")
+                        .requestMatchers("/employees/view/**").hasRole("employee")
                         .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
-                                .jwkSetUri("http://localhost:8080/realms/master/protocol/openid-connect/certs")))
+                                .jwkSetUri("http://localhost:9091/realms/SpringBootKeycloak/protocol/openid-connect/certs")))
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .permitAll());
+
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS));
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+
         return http.build();
     }
 }
