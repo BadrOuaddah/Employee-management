8130384df17103e8a64b911eea074642989a7cda
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-09-12 02:22:18 +0100

    Create service layer

---
+++ src/main/java/com/example/EmployeeManagement/service/ManagerService.java
@@ -0,0 +1,41 @@
+package com.example.EmployeeManagement.service;
+
+import com.example.EmployeeManagement.entity.Manager;
+import com.example.EmployeeManagement.implementation.ManagerImpl;
+import com.example.EmployeeManagement.repository.ManagerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ManagerService implements ManagerImpl {
+
+    private final ManagerRepository managerRepository;
+
+    public ManagerService(ManagerRepository managerRepository) {
+        this.managerRepository = managerRepository;
+    }
+
+    public List<Manager> getAllManages() {
+        return managerRepository.findAll();
+    }
+
+    public Optional<Manager> getManager(long managerId) {
+        return managerRepository.findById(managerId);
+    }
+
+    public Manager addNewManager(Manager manager) {
+        return managerRepository.save(manager);
+    }
+
+    public Manager updateManager(Long managerId, Manager manager) throws Exception {
+        managerRepository.findById(managerId).orElseThrow(() -> new Exception("Manager not found with id : " + managerId));
+        return managerRepository.save(manager);
+    }
+
+    public void deleteManager(Long managerId) throws Exception {
+        managerRepository.findById(managerId).orElseThrow(() -> new Exception("Manager not found with id : " + managerId));
+        managerRepository.deleteById(managerId);
+    }
+}
