3992f9b7aec9b60a1a8384901d747a3e3e491d52
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-09-19 15:24:25 +0100

    Add @NoRepositoryBean annotation in repository layer

--- src/main/java/com/example/EmployeeManagement/base/BaseRepository.java
+++ src/main/java/com/example/EmployeeManagement/base/BaseRepository.java
@@ -2,6 +2,8 @@ package com.example.EmployeeManagement.base;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface BaseRepository<ENTITY extends BaseEntity, TYPEID extends Object> extends JpaRepository<ENTITY, TYPEID>, JpaSpecificationExecutor<ENTITY> {
 }
