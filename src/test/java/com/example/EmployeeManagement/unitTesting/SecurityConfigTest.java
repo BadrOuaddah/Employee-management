81c9222d8574677e57a31c836254ed514f025bc6
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-09-25 00:11:54 +0100

    Add test methods for roles access

--- src/test/java/com/example/EmployeeManagement/unitTesting/SecurityConfigTest.java
+++ src/test/java/com/example/EmployeeManagement/unitTesting/SecurityConfigTest.java
@@ -1,9 +1,12 @@
 package com.example.EmployeeManagement.unitTesting;
 
+import com.example.EmployeeManagement.configuration.JwtConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,6 +18,9 @@ public class SecurityConfigTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private JwtConverter jwtConverter;
 
     @Test
     public void accessSwaggerUITest() throws Exception {
@@ -28,4 +34,24 @@ public class SecurityConfigTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    @WithMockUser(username = "adminUser", roles = {"admin"})
+    public void adminRoleAccessTest() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "managerUser", roles = {"manager"})
+    public void managerRoleAccessTest() throws Exception {
+        mockMvc.perform(get("/managers"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "unauthorizedUser", roles = {"employee"})
+    public void unauthorizedAccessTest() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isForbidden());
+    }
 }
