8699451e64accc4a9b954639b1e7b067af686688
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-09-30 11:23:45 +0100

    Add Token Url

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,8 +1,13 @@
-function fun(){
-
+function fun() {
     var config = {
-            baseUrl : "http://localhost:8080"
+        baseUrl: 'http://localhost:8080',
+        tokenUrl: 'http://localhost:9091/realms/SpringBootKeycloak/protocol/openid-connect/token'
     };
+
+    var result = karate.callSingle('classpath:karate/token.feature', config);
+    config.authToken = result.token;
+
+    karate.configure('headers', { Authorization: 'Bearer ' + config.authToken });
 
     return config;
 }
