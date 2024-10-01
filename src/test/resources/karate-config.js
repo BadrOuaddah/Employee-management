0a202ab8b431c64d50725580bb939aa895936473
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-10-01 13:41:40 +0100

    Add some variables in karate-config.js

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,11 +1,18 @@
 function fun() {
     var config = {
         baseUrl: 'http://localhost:8080',
-        tokenUrl: 'http://localhost:9091/realms/SpringBootKeycloak/protocol/openid-connect/token'
+        tokenUrl: 'http://localhost:9091/realms/SpringBootKeycloak/protocol/openid-connect/token',
+        clientId: 'employee-app',
+        clientSecret: 'myClientSecret',
+        users: {
+            adminUser: { username: 'adminUser', password: 'password' },
+            managerUser: { username: 'managerUser', password: 'password' },
+            employeeUser: { username: 'employeeUser', password: 'password' }
+        }
     };
 
     var result = karate.callSingle('classpath:karate/token.feature', config);
-    config.authToken = result.token;
+    config.authToken = result.authToken;
 
     karate.configure('headers', { Authorization: 'Bearer ' + config.authToken });
 
