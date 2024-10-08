8ec41d193318fc9656ce32a3c9ce0a7af24b741b
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2024-10-08 17:10:04 +0100

    Combine three migration files in one

--- src/main/resources/db/migration/V1__create_employee_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employees (
-  id INTEGER PRIMARY KEY,
-  firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  position VARCHAR(255) NOT NULL,
-  department VARCHAR(255) NOT NULL
-);
--- src/main/resources/db/migration/V2__create_manager_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE managers (
-  id INTEGER PRIMARY KEY,
-  firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL
-);
